cdc1156028ef86d7ccf880c5b2ad006c358ac7f8
Olster1 <oliver_marsh@live.com.au>
2017-03-02 17:23:45 +1100

    finished making an animated menu bar

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
   $db_file = fopen("posts.txt", "r") or die("unable to open file");
 
   $posts = "[";
@@ -40,28 +46,31 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Homemade+Apple|Marck+Script|Montserrat|Poppins:400|Raleway|Reenie+Beanie" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style1.css">
   </head>
   <body>
   
   <div style="width: 100%; max-width: 100%; overflow: hidden;">
     <img id="background-image" src="jessie-renee-website-background.jpg" />
+      
+      <button style="z-index: 2;" id='burger-btn' class='burger-btn'>&#9776</button>
       <div id="nav-bar"></div>
+
       <div id="book-carousel">
-        <div style="position: absolute; bottom: 5%; left: 80%; z-index: 1"><button id="left-scroll-btn"><span><</span></button><button id="right-scroll-btn"><span>></span></button></div>
+        <div style="position: absolute; bottom: 20%; left: 80%; z-index: 0"><button class="scroll-btn" id="left-scroll-btn"><span><</span></button><button class="scroll-btn" id="right-scroll-btn"><span>></span></button></div>
       </div>
-      <div id="signup-form" style="position: fixed; width: 30em; bottom: -100em;">
+      <div id="signup-form" style="position: fixed; width: 40em; bottom: -100em;">
         <img src="Pop up.png" style="position: absolute; width: 100%; z-index: 0;"/>
-        <div style="text-align: left; display: inline-block; position: relative; top: 3em; left: 14em; width: 50%; font-family: 'Raleway', sans-serif; color: white;">
-        <button id="signup-form-exit-btn" style="position: absolute; border-radius: 1em; padding-left: 0.2em; padding-right: 0.2em; font-size: 14pt; background-color: gray; border: none; top: -0.8em; color: black; right: -0.8em;">X</button>
+        <div style="text-align: left; position: absolute; right: 0; width: 50%; top: 4em; font-family: 'Raleway', sans-serif; color: white;">
+        <button id="signup-form-exit-btn">X</button>
         <form action="http://www.sendy.edgeeffectmedia.com/subscribe" method="POST" accept-charset="utf-8">
           <p style="line-height: 130%;">Signup to get exclusive updates & read Ghost Storm for FREE</p>
           <span style="line-height: 300%">
           <label for="name"><b>Name</b></label>
-          <input style="position: relative; background-color: gray; color: white; border: none; padding-left: 0.5em; padding-right: 0.5em; right: 0;" type="text" name="name" id="name"/>
+          <input class="signup-form-field" type="text" name="name" id="name"/>
           <br/>
           <label for="email"><b>Email</b></label>
-          <input style="position: relative; background-color: gray; color: white; border: none; padding-left: 0.5em; padding-right: 0.5em; left: 0.3em;" type="email" name="email" id="email"/>
+          <input class="signup-form-field" type="email" name="email" id="email"/>
           <br/>
           <input type="hidden" name="list" value="PsWbB1YsrVSedE6TE9jD8w"/>
           <div style="text-align: center"><input style="border: none; background-color: white; padding-left: 0.5em; padding-right: 0.5em; font-weight: bold;" type="submit" name="submit" value="Get my Free Book" id="submit"/></div>
@@ -72,7 +81,7 @@
         </div>
       </div>
       <div id="social-media" style="text-align: center;"></div>
-      <div id="blog-posts" style="display: inline-block; width: 45%; vertical-align: top"></div><!-- LightWidget WIDGET --><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/c0b9af16f9a35017819997a7a7cee54c.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden; width: 45%;"></iframe>
+      <div id="blog-posts" style="display: inline-block; width: 45%; vertical-align: top">
       <div id="home-section"></div>
 
   </div>
@@ -98,11 +107,28 @@ function lerp(A, t, B) {
   return (B - A)*t + A;
 }
 
+
+var MOVE_LEFT  = 1; 
+var MOVE_RIGHT = 2; 
+var MOVE_UP  = 3; 
+var MOVE_DOWN = 4; 
+
+var SHOW = 1;
+var HIDE = 2;
+
+var FORM_BOX_INVISIBLE = -0.1;
+var FORM_BOX_VISIBLE = 0.5;
+
+var LEFT_PERCENT = -1;
+var MIDDLE_PERCENT = 0;
+var RIGHT_PERCENT = 1.2;
+
 window.onload = function() {
 
-  function createNavLinkString(name, extraClassName, isInline) {
+  function createNavLinkString(name, extraClassName, isInline, id) {
     var inlineValue = (isInline) ? "inline" : "block";
-    return "<a class='nav-link " + extraClassName + "' id='nav-item-" + name + "' href='./#" + name + "'><div style='display:" + inlineValue +"; position: relative;'>" + name + "</div></a>";
+    var Result  = "<a class='nav-link " + extraClassName + "' id='nav-item-" + name + "' href='./#" + name + "'><div style='display:" + inlineValue +"; position: relative;'>" + name + "</div></a>";
+    return Result;
   }
   var navLinks = ["Home", "Books", "About", "Contact", "Blog"];
   var books = ["Hidden Bay", "White City"];
@@ -110,15 +136,18 @@ window.onload = function() {
   var navBar = document.getElementById("nav-bar");
   var navLinkString = "";
   for(var i = 0; i < navLinks.length; i++) {
-    navLinkString += createNavLinkString(navLinks[i], "", true); 
     if(navLinks[i] === "Books") {
+      navLinkString += createNavLinkString(navLinks[i], "", true, "book-menu-btn"); 
       navLinkString += "<div class='drop-down-menu' id='drop-down-menu-" + navLinks[i] + "'>";
       for(var booksIndex = 0; booksIndex < books.length; booksIndex++) {
-        navLinkString += createNavLinkString(books[booksIndex], "nav-link-drop-down", false);
+        navLinkString += createNavLinkString(books[booksIndex], "nav-link-drop-down", false, "");
       }
       navLinkString += "</div>";
-    } 
+    } else {
+      navLinkString += createNavLinkString(navLinks[i], "", true, ""); 
+    }
   }
+  
   navBar.innerHTML = navLinkString;
 
   var socialMedia = [
@@ -140,21 +169,6 @@ window.onload = function() {
     blogElm.innerHTML += "<div style='padding: 2%;'>" + blogPosts[i].date + "<a href='./blog/" + blogPosts[i].title + "'>" + blogPosts[i].title + "</a>" + blogPosts[i].text + "</div>";
   }  
 
-  var MOVE_LEFT  = 1; 
-  var MOVE_RIGHT = 2; 
-  var MOVE_UP  = 3; 
-  var MOVE_DOWN = 4; 
-
-  var SHOW = 0;
-  var HIDE = 1;
-
-  var FORM_BOX_INVISIBLE = -0.3;
-  var FORM_BOX_VISIBLE = 0.16;
-  
-  var LEFT_PERCENT = -1;
-  var MIDDLE_PERCENT = 0;
-  var RIGHT_PERCENT = 1.2;
-
   var animations = [];
 
   var carouselContent = [
@@ -163,8 +177,8 @@ window.onload = function() {
   {text: "It was foolish. Taking off her engagement ring for just one night. It wasn’t meant to become anything…</p><p>But now fate won’t let Ashley forget Ethan, even if she wanted to.</p><a href='./crossing_paths'>Read More</a>", imageUrl: "crossing_paths.png"}];
 
 
-  function addAnimation(tPeriod, objectToModify, variableName, type, domObject) {
-  	var object = {tAt: 0, period: tPeriod, minValue: 0, maxValue: 0, object: objectToModify, variableName: variableName, callBack: null, type: type, domObject: domObject, updating: false};
+  function addAnimation(tPeriod, objectToModify, variableName, type, units, domObject) {
+  	var object = {tAt: 0, period: tPeriod, minValue: 0, maxValue: 0, object: objectToModify, units: units, variableName: variableName, callBack: null, type: type, domObject: domObject, updating: false, isOpen: false};
   	var lengthOfArray = animations.push(object);
   	return (lengthOfArray - 1);
   }
@@ -172,7 +186,8 @@ window.onload = function() {
   function createCarouselItem(index, xPercent) {
     var carElm = carouselContent[index];
     var startX = (xPercent*100) + "%";
-    var styleString = "position: relative; padding-right: 2%; padding-left: 2%; display: inline-block; width: 40%; vertical-align: top; top: 5vw; text-align: left; font-size: 1.8vw; line-height: 100%;";
+    var styleString = "position: relative; padding-right: 2%; padding-left: 2%; display: inline-block; width: 40%; vertical-align: top; top: 5vw; text-align: left; font-size: 1.8vw; minline-height: 100%;";
+    //Todo make font size reach  minimum size;
 
     return "<span id='carousel" + index + "' style='text-align: center; position: absolute;display: inline-block; width: 100%; left: " + startX + "'><div style='" + styleString + "'>" + carElm.text + "</div>" + "<img style='height: 30vw; ' src='" + carElm.imageUrl + "'></span>";
   }
@@ -190,14 +205,17 @@ window.onload = function() {
   for(var i = 0; i < carouselContent.length; ++i) {
   	var thisCarousel = document.getElementById("carousel" + i)
   	var tPeriod = 3.0;
-	  addAnimation(tPeriod, thisCarousel.style, "left", "carousel", null, null);
+	  addAnimation(tPeriod, thisCarousel.style, "left", "carousel", "%", null);
   }
 
 //TODO: make a spot to put second parameter in addAnimation()
-  var FormIndex = addAnimation(0.4, document.getElementById("signup-form").style, "bottom", "", null);
+  var FormIndex = addAnimation(0.4, document.getElementById("signup-form").style, "bottom", "", "%", null);
 
-  var BookDropDownIndex = addAnimation(3.0, document.getElementById("drop-down-menu-Books").style, "height", "", document.getElementById("drop-down-menu-Books"));
+  var BookDropDownIndex = addAnimation(0.5, document.getElementById("drop-down-menu-Books").style, "height", "", "px", document.getElementById("drop-down-menu-Books"));
 
+  var navElm = document.getElementById("nav-bar");
+  var NavBarAnimationIndex = addAnimation(1.0, navElm.style, "height", "", "px", navElm);
+  
   function moveItemOn(moveDirection) {
     autoChangeTimer = 0;
 
@@ -242,7 +260,54 @@ window.onload = function() {
     }
   }
 
+  function triggerHeightAnimation(IndexValue, latestState) {
+    var anim =  animations[IndexValue];
+    var animElm = anim.domObject;
+    if(anim.isOpen) {
+      latestState.value = HIDE;
+    } else {
+      latestState.value = SHOW;
+    }
+
+    if(!anim.updating) {
+      animElm.style.height = "100%";
+      animElm.style.visibility = "visible";  
+      animElm.style.display = "block";  
+      var height = parseInt(window.getComputedStyle(animElm, null).getPropertyValue("height"), 10);
+      if(anim.isOpen) {
+          anim.minValue = height;
+          anim.maxValue = 0;
+          anim.updating = true;
+          anim.isOpen = false;
+          anim.callback = null;
+          anim.callback = function() {
+            //animElm.style.height = "0";
+            if(latestState.value === SHOW) {
+              triggerHeightAnimation(IndexValue, latestState);
+            }
+          };
+      } else {    
+        animElm.style.height = 0;
+        anim.minValue = 0;
+        anim.maxValue = height;
+        anim.updating = true;
+        anim.isOpen = true;
+        anim.callback = function() {
+          animElm.style.height = "100%";
+          if(latestState.value === HIDE) {
+            triggerHeightAnimation(IndexValue, latestState);
+          }
+          
+        };
+      }
+    }
+  }
+
   //////////////////////
+
+  var navBarLatestState = {value: 0};
+  var booksMenuLatestState = {value: 0};
+  var BookMenuWasOpen = false;
 
   var signupExit = document.getElementById("signup-form-exit-btn");
   signupExit.addEventListener("click", function(e) {
@@ -254,6 +319,25 @@ window.onload = function() {
     moveItemOn(MOVE_LEFT);
   });
 
+  var burgerBtn = document.getElementById("burger-btn");
+  burgerBtn.addEventListener("click", function(e) {
+    triggerHeightAnimation(NavBarAnimationIndex, navBarLatestState);
+    if(animations[BookDropDownIndex].isOpen) {
+      triggerHeightAnimation(BookDropDownIndex, booksMenuLatestState);
+      BookMenuWasOpen = true;
+    } else if(BookMenuWasOpen) {
+      //triggerHeightAnimation(BookDropDownIndex, booksMenuLatestState);  
+      BookMenuWasOpen = false;
+    }
+    
+
+  });
+
+  var subMenuBtn = document.getElementById("nav-item-Books");
+  subMenuBtn.addEventListener("click", function(e) {
+    triggerHeightAnimation(BookDropDownIndex, booksMenuLatestState);
+  });
+
   var rightBtn = document.getElementById("right-scroll-btn");
   rightBtn.addEventListener("click", function(e) {
     moveItemOn(MOVE_RIGHT);
@@ -263,67 +347,56 @@ window.onload = function() {
   var isHovering1 = false;
 
 
-  var booksNavItem = document.getElementById("nav-item-Books");
-  booksNavItem.addEventListener("mouseover", function(e) {
-    toggleDropDownMenu(SHOW);
-    isHovering0 = true;
-  });
-  booksNavItem.addEventListener("mouseout", function(e) {
-    isHovering0 = false;
-    if(!isHovering0 && !isHovering1) toggleDropDownMenu(HIDE);
-  });
+  // var booksNavItem = document.getElementById("nav-item-Books");
+  // booksNavItem.addEventListener("mouseover", function(e) {
+  //   toggleDropDownMenu(SHOW);
+  //   console.log("show1");
+  //   isHovering0 = true;
+  // });
+  // booksNavItem.addEventListener("mouseout", function(e) {
+  //   isHovering0 = false;
+  //   console.log("hide1");
+  //   if(!isHovering0 && !isHovering1) toggleDropDownMenu(HIDE);
+  // });
 
-  var menuItem = document.getElementById("drop-down-menu-Books");
-  menuItem.addEventListener("mouseover", function(e) {
-    isHovering1 = true;
-    toggleDropDownMenu(SHOW);
-  });
-  menuItem.addEventListener("mouseout", function(e) {
-    isHovering1 = false;
-    if(!isHovering0 && !isHovering1) toggleDropDownMenu(HIDE);
-  });
+  // var menuItem = document.getElementById("drop-down-menu-Books");
+  // menuItem.addEventListener("mouseover", function(e) {
+  //   isHovering1 = true;
+  //   toggleDropDownMenu(SHOW);
+  //   console.log("show2");
+  // });
+  // menuItem.addEventListener("mouseout", function(e) {
+  //   isHovering1 = false;
+  //   console.log("hide2");
+  //   if(!isHovering0 && !isHovering1) toggleDropDownMenu(HIDE);
+  // });
 
-
-  function getOppositeState(state) {
-    var res = MOVE_NULL;
-    if(state === SHOW) {
-      res = MOVE_DOWN;
-    } else if(state === HIDE) {
-      res = MOVE_UP;
-    }
-    return res;
-  }
-
+ var mobileView = false;
   function toggleDropDownMenu(state) {
-      // if(currentDropDownState !== MOVE_NULL && currentDropDownState !== getOppositeState(state)) {
-      //   dropDownMenu_t = 1.0 - dropDownMenu_t;
-      // }
-      var DropDownAnim = animations[BookDropDownIndex];
-      console.log(DropDownAnim);
-      DropDownAnim.domObject.style.display = "inline";
-      DropDownAnim.tAt = 0.0;
-      console.log(state);
+    console.log(mobileView);
+    if(mobileView) {  
+      latestState = state;
+      triggerHeightAnimation(BookDropDownIndex, true);
+    } else {
+      var menuElm = document.getElementById("drop-down-menu-Books");
       if(state === SHOW) {
-        DropDownAnim.minValue = 0;
-        DropDownAnim.maxValue = 1;
-      } else if(state === HIDE) {
-        DropDownAnim.minValue = 1;
-        DropDownAnim.maxValue = 0;
+        menuElm.style.visibility = "visible";
+        menuElm.style.display = "block";
+        menuElm.style.height = "100%";
+      } else if(state == HIDE) {
+        menuElm.style.visibility = "hidden";
+        menuElm.style.display = "none";
+        menuElm.style.height = "0%";
       }
-
-      DropDownAnim.updating = true;
-      DropDownAnim.callBack = function() {
-        if(DropDownAnim.maxValue === 0) {
-            DropDownAnim.domObject.style.display = "none";
-          } 
-      };
+    }
+      
   }
 
   var animationTime = 1.0/30.0;
 
   window.setTimeout(function(){ 
     beginFormMove(MOVE_UP);
-  }, 2000);
+  }, 0);
 
   function beginFormMove(move) {
     var FormAnimation = animations[FormIndex];
@@ -338,21 +411,29 @@ window.onload = function() {
     FormAnimation.updating = true;
   }
 
+  function getValueAt(minValue, t, maxValue, period) {
+    return (lerp(minValue, tLinear_to_tSineous(t / period), maxValue));
+  }
+
   function getPercentValueAt(minValue, t, maxValue, period) {
-    return (100*lerp(minValue, tLinear_to_tSineous(t / period), maxValue)) + "%";
+    return (100*lerp(minValue, tLinear_to_tSineous(t / period), maxValue));
   }
 
   var autoChangeTimer = 0;
   window.setInterval(function(){
     var updateCarousel = true;
+    //console.log(document.getElementById("nav-bar").style.height);
   	for(var i = 0; i < animations.length; ++i) {
 
   		var anim = animations[i];
   		if(anim.updating) {
   			anim.tAt += animationTime;
-	  		anim.object[anim.variableName] = getPercentValueAt(anim.minValue, anim.tAt, anim.maxValue, anim.period);
+        var value = (anim.units === "%") ? getPercentValueAt(anim.minValue, anim.tAt, anim.maxValue, anim.period) : getValueAt(anim.minValue, anim.tAt, anim.maxValue, anim.period);
+	  		anim.object[anim.variableName] = value + anim.units;
 	  		if((anim.tAt / anim.period) > 1) {
 	  		    anim.tAt = 0;
+            var finalValue = (anim.units === "%") ? anim.maxValue*100 : anim.maxValue;
+            anim.object[anim.variableName] = finalValue + anim.units;
 	  		    anim.updating = false;
 	  		    if(anim.callback) {
 	  		    	anim.callback();
@@ -401,12 +482,21 @@ window.onload = function() {
   function mediaQueryResponse (mq) {
     if (mq.matches) {
       setDropDownMenuItemsPosition("static");
+      mobileView = true;
+      var navBar = document.getElementById("nav-bar");
+      navBar.style.overflow = "hidden";
+      document.getElementById("burger-btn").style.display = "block";
+
     } else {
       setDropDownMenuItemsPosition("absolute");
+      document.getElementById("burger-btn").style.display = "none";
+      var navBar = document.getElementById("nav-bar");
+      navBar.style.overflow = "visible";
+      mobileView = false;
     }  
   }
 
-  var mq = window.matchMedia('screen and (max-width: 820px)');
+  var mq = window.matchMedia('screen and (max-width: 920px)');
   mediaQueryResponse(mq);
   mq.addListener(mediaQueryResponse);
 }
